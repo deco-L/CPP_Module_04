8259cf19710f46818acc058ff2f18d2e27b3bbf5
deco-L <scotchwitchbroom@gmail.com>
2024-05-23 13:08:36 +0900

    V V

---
+++ ex02/include/Dog.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Dog.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
+/*   Updated: 2024/05/18 18:11:25 by csakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#ifndef DOG_HPP
+#define DOG_HPP
+
+#include "Animal.hpp"
+#include "Brain.hpp"
+
+class Dog : public Animal
+{
+private:
+  Brain*  brain;
+
+public:
+  Dog( void );
+  Dog( const Dog& obj );
+  virtual ~Dog( void );
+
+  void  makeSound( void ) const;
+
+  Dog&  operator=( const Dog& obj );
+};
+
+#endif
---
+++ ex02/src/main.cpp
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
+/*   Updated: 2024/05/09 16:35:50 by csakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+#include "Cat.hpp"
+#include "Dog.hpp"
+#include "WrongAnimal.hpp"
+#include "WrongCat.hpp"
+#include <cstdlib>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+static size_t get_term_line() {
+	winsize	terminal_size;
+	ioctl(STDOUT_FILENO, TIOCGWINSZ, &terminal_size);
+	return (terminal_size.ws_col);
+}
+
+static void draw_terminal_line() {
+	size_t	terminal_col;
+
+	terminal_col = get_term_line();
+	for (size_t i = 0; i < terminal_col; i++)
+		std::cout << "=";
+	std::cout << std::endl;
+	return ;
+}
+
+int main(void) {
+  draw_terminal_line();
+  const Animal  *j = new Dog();
+  const Animal  *i = new Cat();
+  std::cout << j->getType() << " " << std::endl;
+  std::cout << i->getType() << " " << std::endl;
+  i->makeSound(); //will output the cat sound!
+  j->makeSound();
+  delete j;
+  delete i;
+  draw_terminal_line();
+
+  draw_terminal_line();
+  const WrongAnimal*  metameta = new WrongAnimal();
+  const WrongAnimal*  k = new WrongCat();
+  std::cout << metameta->getType() << " " << std::endl;
+  std::cout << k->getType() << " " << std::endl;
+  metameta->makeSound();
+  k->makeSound();
+  delete metameta;
+  delete k;
+  draw_terminal_line();
+  return (EXIT_SUCCESS);
+}
